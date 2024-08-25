59043208c05b84fd4a8b99a1b5bdddef636ffa2f
Weiyi Lee <weiyilee17@gmail.com>
2024-08-25 21:25:45 +0800

    Change default theme to dark theme

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -58,6 +58,10 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
+    colorMode: {
+      defaultMode: 'dark',
+      // disableSwitch: true,
+    },
     navbar: {
       title: 'Home',
       logo: {
